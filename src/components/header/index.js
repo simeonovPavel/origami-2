693b1e05d54c29b5ac74bd7a4666392d1b1ab026
simeonovPavel <43511823+simeonovPavel@users.noreply.github.com>
2021-01-24 21:57:13 +0200

    test

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <ul>
+                <li>tab 1</li>
+                <li>tab 2</li>
+                <li>tab 3</li>
+                <li>tab 4</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
+
+
