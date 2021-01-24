9576914dc53e17ad756221fecbd83c7a5c45fe8c
simeonovPavel <43511823+simeonovPavel@users.noreply.github.com>
2021-01-24 22:41:21 +0200

    test 2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from './index.module.css'
+import Link from '../link'
 
 const Header = () => {
     return (
-        <header className="App-header">
-            <ul>
-                <li>tab 1</li>
-                <li>tab 2</li>
-                <li>tab 3</li>
-                <li>tab 4</li>
-            </ul>
+        <header className={styles.navigation}>
+            <nav>
+                <Link />
+                <Link />
+                <Link />
+            </nav>
         </header>
     )
 }
---
+++ src/components/link/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.css'
+
+const Link = () => {
+    return (
+        <li className={styles.listItem}>
+            <a href="#">Tab 1</a>
+        </li>
+    )
+}
+
+export default Link;
